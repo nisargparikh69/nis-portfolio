b431b4e9ade7c8e6e904b9dfa683cddc615979d4
Nisarg Parikh <nisargparikh69@gmail.com>
2024-03-28 20:14:15 +0530

    update focus route

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,69 +1,122 @@
-import { ProjectCard } from "./ProjectCard";
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import colorSharp2 from "../assets/img/color-sharp2.png";
+import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
 export const Projects = () => {
   const projects = [
     {
-      name: "Project 1",
-      description: "This is project 1",
+      title: "Business Startup",
+      description: "Design & Development",
       imgUrl: projImg1,
     },
     {
-      name: "Project 2",
-      description: "This is project 2",
+      title: "Business Startup",
+      description: "Design & Development",
       imgUrl: projImg2,
     },
     {
-      name: "Project 3",
-      description: "This is project 3",
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
       imgUrl: projImg3,
     },
   ];
+
   return (
-    <section className="project" id="project">
+    <section className="project" id="projects">
       <Container>
         <Row>
-          <Col>
-            <h2>Projects</h2>
-            <p>
-              Lorem Ipsum Description dummy text for printing and demoing
-              <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                <Nav
-                  variant="pills"
-                  defaultActiveKey="/home"
-                  className="nav-pills mb-5 justify-content-center align-items-center"
-                  id="pills-tab"
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
                 >
-                  <Nav.Item>
-                    <Nav.Link eventKey="first">Tab1</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="second">Tab2</Nav.Link>
-                  </Nav.Item>
-                  <Nav.Item>
-                    <Nav.Link eventKey="third">Tab3</Nav.Link>
-                  </Nav.Item>
-                </Nav>
-                <Tab.Content>
-                  <Tab.Pane eventKey="first">
-                    <Row>
-                      {projects.map((_project, index) => {
-                        return <ProjectCard key={index} {...projects} />;
-                      })}
-                    </Row>
-                  </Tab.Pane>
-                  <Tab.Pane eventKey="second">Lorem Ipsum 2</Tab.Pane>
-                  <Tab.Pane eventKey="third">Lorem Ipsum 3</Tab.Pane>
-                </Tab.Content>
-              </Tab.Container>
-            </p>
+                  <h2>Projects</h2>
+                  <p>
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it to make a
+                    type specimen book.
+                  </p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav
+                      variant="pills"
+                      className="nav-pills mb-5 justify-content-center align-items-center"
+                      id="pills-tab"
+                    >
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content
+                      id="slideInUp"
+                      className={
+                        isVisible ? "animate__animated animate__slideInUp" : ""
+                      }
+                    >
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {projects.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="section">
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Cumque quam, quod neque provident velit, rem
+                          explicabo excepturi id illo molestiae blanditiis,
+                          eligendi dicta officiis asperiores delectus quasi
+                          inventore debitis quo.
+                        </p>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        <p>
+                          Lorem ipsum dolor sit amet consectetur adipisicing
+                          elit. Cumque quam, quod neque provident velit, rem
+                          explicabo excepturi id illo molestiae blanditiis,
+                          eligendi dicta officiis asperiores delectus quasi
+                          inventore debitis quo.
+                        </p>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              )}
+            </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      <img src={colorSharp2} className="background-image-right"></img>
+      <img className="background-image-right" src={colorSharp2}></img>
     </section>
   );
 };
